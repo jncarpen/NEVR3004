354e13b656c1d725b5c1fb14a93028cc6f83a5af
Jo Carpenter <jordannc@stud.ntnu.no>
2020-06-05 20:12:18 +0200

    makeSampleTrajectories

--- Hopfield/makeSampleTrajectories.m
+++ Hopfield/makeSampleTrajectories.m
@@ -3,8 +3,15 @@
 % Jo Carpenter
 % Last modified: June 5, 2020
 
-figure
-for i=1:10:1000
-plot(ConvergenceData{1,3}{i,3}, 'LineWidth', 1.25)
-hold on
+proportionNoise = [0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1];
+for pattern = 1:6 
+    for noise_iter = 1:11 % loop through all possible noise values
+        subplot(3,4,noise_iter)
+        title(sprintf('%d percent noise',proportionNoise(noise_iter)*100))
+
+        for i=1:30:1000
+            plot(ConvergenceData{1,3}{i,noise_iter}, 'LineWidth', 1.25)
+            hold on
+        end
+    end
 end
